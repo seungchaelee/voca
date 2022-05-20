5e5937f86b4d0a92ba9e14a2c96006a9ff5f708f
seungchae <id6639@naver.com>
2022-05-20 14:56:12 +0900

    new voca

---
+++ src/component/Welcome.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Welcome() {
+  return (
+    <h2>
+      Welcome tooooo.
+    </h2>
+  );
+}
---
+++ src/component/world.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Hello from "./Hello";
+import Welcome from "./Welcome";
+
+export default function World() {
+  return (
+    <>
+      <Hello />
+      <Welcome />
+    </>
+  );
+}
