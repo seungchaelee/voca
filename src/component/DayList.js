249db4bd2462d240ae298f4a339315071c64ef64
seungchae <id6639@naver.com>
2022-06-04 21:35:32 +0900

    del useState

--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 import Loading from "./Loading";
